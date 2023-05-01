aca161ec202ff176e5adf931cd4608506c2c7147
Dzianis Perahudau <tvigger@yandex.ru>
2023-05-02 01:17:21 +0300

    feat: add the ability to switch keyboard layouts between English and Russian

--- index.js
+++ index.js
@@ -2,19 +2,61 @@ function stringFromCharCode(num) {
   return String.fromCharCode(num);
 };
 
-const keyboardKeysLineOne = {'+192':"`", '+49':stringFromCharCode(49), '+50':stringFromCharCode(50), '+51':stringFromCharCode(51), '+52':stringFromCharCode(52), '+53':stringFromCharCode(53), '+54':stringFromCharCode(54), '+55':stringFromCharCode(55), '+56':stringFromCharCode(56), '+57':stringFromCharCode(57), '+48':stringFromCharCode(48), '+189':"-", '+187':"=", '+8':"Backspace"};
-const keyboardKeysLineTwo = {'+9':"Tab", '+81':stringFromCharCode(81), '+87':stringFromCharCode(87), '+69':stringFromCharCode(69), '+82':stringFromCharCode(82), '+84':stringFromCharCode(84), '+89':stringFromCharCode(89), '+85':stringFromCharCode(85), '+73':stringFromCharCode(73), '+79':stringFromCharCode(79), '+80':stringFromCharCode(80), '+219':"[", '+221':"]", '+220':"Backslash", '+46':"Delete"};
-const keyboardKeysLinethree = {'+20':"CapsLock", '+65':stringFromCharCode(65), '+83':stringFromCharCode(83), '+68':stringFromCharCode(68), '+70':stringFromCharCode(70), '+71':stringFromCharCode(71), '+72':stringFromCharCode(72), '+74':stringFromCharCode(74), '+75':stringFromCharCode(75), '+76':stringFromCharCode(76), '+186':";", '+222':"'", '+13':"Enter"};
-const keyboardKeysLinefour = {'+16':"Shift", '+90':stringFromCharCode(90), '+88':stringFromCharCode(88), '+67':stringFromCharCode(67), '+86':stringFromCharCode(86), '+66':stringFromCharCode(66), '+78':stringFromCharCode(78), '+77':stringFromCharCode(77), '+188':",", '+190':".", '+191':"/", '+1616':"ShiftRight"};
-const keyboardKeysLinefive = {'+17':"Ctrl", '+91':"Meta", '+18':"Alt", '+32':"Space", '+1818':"AltRight", '+1717':"ControlRight", '+37':"ArrowLeft", '+38':"ArrowUp", '+40':"ArrowDown", '+39':"ArrowRight"};
-/*let keyboardKeys = [];
-let bodyArea = document.querySelector('body');
-bodyArea.onkeydown = function(event) {
-  // console.log(event);
-  keyboardKeys.push(event.keyCode);
-  console.log(event/*.keyCode,);
-}*/
+const keyboardKeysLineOneEn = {'+192':"`", '+49':stringFromCharCode(49), '+50':stringFromCharCode(50), '+51':stringFromCharCode(51), '+52':stringFromCharCode(52), '+53':stringFromCharCode(53), '+54':stringFromCharCode(54), '+55':stringFromCharCode(55), '+56':stringFromCharCode(56), '+57':stringFromCharCode(57), '+48':stringFromCharCode(48), '+189':"-", '+187':"=", '+8':"Backspace"};
+const keyboardKeysLineTwoEn = {'+9':"Tab", '+81':stringFromCharCode(81), '+87':stringFromCharCode(87), '+69':stringFromCharCode(69), '+82':stringFromCharCode(82), '+84':stringFromCharCode(84), '+89':stringFromCharCode(89), '+85':stringFromCharCode(85), '+73':stringFromCharCode(73), '+79':stringFromCharCode(79), '+80':stringFromCharCode(80), '+219':"[", '+221':"]", '+220':"Backslash", '+46':"Delete"};
+const keyboardKeysLinethreeEn = {'+20':"CapsLock", '+65':stringFromCharCode(65), '+83':stringFromCharCode(83), '+68':stringFromCharCode(68), '+70':stringFromCharCode(70), '+71':stringFromCharCode(71), '+72':stringFromCharCode(72), '+74':stringFromCharCode(74), '+75':stringFromCharCode(75), '+76':stringFromCharCode(76), '+186':";", '+222':"'", '+13':"Enter"};
+const keyboardKeysLinefourEn = {'+16':"Shift", '+90':stringFromCharCode(90), '+88':stringFromCharCode(88), '+67':stringFromCharCode(67), '+86':stringFromCharCode(86), '+66':stringFromCharCode(66), '+78':stringFromCharCode(78), '+77':stringFromCharCode(77), '+188':",", '+190':".", '+191':"/", '+1616':"ShiftRight"};
+const keyboardKeysLinefiveEn = {'+17':"Ctrl", '+91':"Meta", '+18':"Alt", '+32':"Space", '+1818':"AltRight", '+1717':"ControlRight", '+37':"ArrowLeft", '+38':"ArrowUp", '+40':"ArrowDown", '+39':"ArrowRight"};
 
+const keyboardKeysLineOneRu = {'+192':"`", '+49':stringFromCharCode(49), '+50':stringFromCharCode(50), '+51':stringFromCharCode(51), '+52':stringFromCharCode(52), '+53':stringFromCharCode(53), '+54':stringFromCharCode(54), '+55':stringFromCharCode(55), '+56':stringFromCharCode(56), '+57':stringFromCharCode(57), '+48':stringFromCharCode(48), '+189':"-", '+187':"=", '+8':"Бекспейс"};
+const keyboardKeysLineTwoRu = {'+9':"Табулирование", '+81':"й", '+87':"ц", '+69':"у", '+82':"к", '+84':"е", '+89':"н", '+85':"г", '+73':"ш", '+79':"щ", '+80':"з", '+219':"[", '+221':"]", '+220':"Backslash", '+46':"Удалить"};
+const keyboardKeysLinethreeRu = {'+20':"КапсЛок", '+65':"ф", '+83':"ы", '+68':"в", '+70':"а", '+71':"п", '+72':"р", '+74':"о", '+75':"л", '+76':"д", '+186':";", '+222':"'", '+13':"Ввод"};
+const keyboardKeysLinefourRu = {'+16':"Левый Шифт", '+90':"я", '+88':"ч", '+67':"с", '+86':"м", '+66':"и", '+78':"т", '+77':"ь", '+188':"б", '+190':"ю", '+191':"/", '+1616':"Правый Шифт"};
+const keyboardKeysLinefiveRu = {'+17':"Контрл", '+91':"Вин", '+18':"Альт", '+32':"Пробел", '+1818':"ПравАльт", '+1717':"ПравКонтрл", '+37':"ArrowLeft", '+38':"ArrowUp", '+40':"ArrowDown", '+39':"ArrowRight"};
+
+
+
+let keyboardKeysLineOne = keyboardKeysLineOneEn;
+ let keyboardKeysLineTwo = keyboardKeysLineTwoEn;
+ let keyboardKeysLinethree = keyboardKeysLinethreeEn;
+ let keyboardKeysLinefour = keyboardKeysLinefourEn;
+ let keyboardKeysLinefive = keyboardKeysLinefiveEn;
+let isLanEn = true;
+
+let flagLang = false;
+document.addEventListener("keydown", function (event) {
+  if (event.code === 'ArrowLeft') {
+    flagLang = true;
+  }else if ((event.code === 'ArrowDown') && (flagLang === true) && (isLanEn === false)){
+  flagLang = false;
+  isLanEn = true;
+  }else if ((event.code === 'ArrowDown') && (flagLang === true) && (isLanEn === true)){
+    flagLang = false;
+    isLanEn = false;
+  
+    }
+});
+
+function translateLang(){
+  if((isLanEn === true)){
+      isLanEn = false;
+      localStorage.setItem('isLanEn', 'true');
+      keyboardKeysLineOne = keyboardKeysLineOneEn;
+      keyboardKeysLineTwo = keyboardKeysLineTwoEn;
+      keyboardKeysLinethree = keyboardKeysLinethreeEn;
+      keyboardKeysLinefour = keyboardKeysLinefourEn;
+      keyboardKeysLinefive = keyboardKeysLinefiveEn;
+  }else if((isLanEn === false)){
+      isLanEn = true;
+      localStorage.setItem('isLanEn', 'false');
+      keyboardKeysLineOne = keyboardKeysLineOneRu;
+      keyboardKeysLineTwo = keyboardKeysLineTwoRu;
+      keyboardKeysLinethree = keyboardKeysLinethreeRu;
+      keyboardKeysLinefour = keyboardKeysLinefourRu;
+      keyboardKeysLinefive = keyboardKeysLinefiveRu;
+  }
+};
+translateLang();
 
 let headerTag = document.querySelector('header');
 const h1Tag = document.createElement('h1');
