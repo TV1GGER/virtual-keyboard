958889af3c035c2645df5a6c8d0492860bbbfade
Dzianis Perahudau <tvigger@yandex.ru>
2023-05-02 02:05:40 +0300

    fix: change the visual layout of the keyboard buttons

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function stringFromCharCode(num) {
 const keyboardKeysLineOneEn = {'+192':"`", '+49':stringFromCharCode(49), '+50':stringFromCharCode(50), '+51':stringFromCharCode(51), '+52':stringFromCharCode(52), '+53':stringFromCharCode(53), '+54':stringFromCharCode(54), '+55':stringFromCharCode(55), '+56':stringFromCharCode(56), '+57':stringFromCharCode(57), '+48':stringFromCharCode(48), '+189':"-", '+187':"=", '+8':"Backspace"};
 const keyboardKeysLineTwoEn = {'+9':"Tab", '+81':stringFromCharCode(81), '+87':stringFromCharCode(87), '+69':stringFromCharCode(69), '+82':stringFromCharCode(82), '+84':stringFromCharCode(84), '+89':stringFromCharCode(89), '+85':stringFromCharCode(85), '+73':stringFromCharCode(73), '+79':stringFromCharCode(79), '+80':stringFromCharCode(80), '+219':"[", '+221':"]", '+220':"Backslash", '+46':"Delete"};
 const keyboardKeysLinethreeEn = {'+20':"CapsLock", '+65':stringFromCharCode(65), '+83':stringFromCharCode(83), '+68':stringFromCharCode(68), '+70':stringFromCharCode(70), '+71':stringFromCharCode(71), '+72':stringFromCharCode(72), '+74':stringFromCharCode(74), '+75':stringFromCharCode(75), '+76':stringFromCharCode(76), '+186':";", '+222':"'", '+13':"Enter"};
-const keyboardKeysLinefourEn = {'+16':"Shift", '+90':stringFromCharCode(90), '+88':stringFromCharCode(88), '+67':stringFromCharCode(67), '+86':stringFromCharCode(86), '+66':stringFromCharCode(66), '+78':stringFromCharCode(78), '+77':stringFromCharCode(77), '+188':",", '+190':".", '+191':"/", '+1616':"ShiftRight"};
-const keyboardKeysLinefiveEn = {'+17':"Ctrl", '+91':"Meta", '+18':"Alt", '+32':"Space", '+1818':"AltRight", '+1717':"ControlRight", '+37':"ArrowLeft", '+38':"ArrowUp", '+40':"ArrowDown", '+39':"ArrowRight"};
+const keyboardKeysLinefourEn = {'+16':"Shift", '+90':stringFromCharCode(90), '+88':stringFromCharCode(88), '+67':stringFromCharCode(67), '+86':stringFromCharCode(86), '+66':stringFromCharCode(66), '+78':stringFromCharCode(78), '+77':stringFromCharCode(77), '+188':",", '+190':".", '+191':"/", '+38':"ArrowUp", '+1616':"ShiftRight"};
+const keyboardKeysLinefiveEn = {'+17':"Ctrl", '+91':"Meta", '+18':"Alt", '+32':"Space", '+1818':"AltRight", '+1717':"ControlRight", '+37':"ArrowLeft", '+40':"ArrowDown", '+39':"ArrowRight"};
 
 const keyboardKeysLineOneRu = {'+192':"`", '+49':stringFromCharCode(49), '+50':stringFromCharCode(50), '+51':stringFromCharCode(51), '+52':stringFromCharCode(52), '+53':stringFromCharCode(53), '+54':stringFromCharCode(54), '+55':stringFromCharCode(55), '+56':stringFromCharCode(56), '+57':stringFromCharCode(57), '+48':stringFromCharCode(48), '+189':"-", '+187':"=", '+8':"Бекспейс"};
 const keyboardKeysLineTwoRu = {'+9':"Табулирование", '+81':"й", '+87':"ц", '+69':"у", '+82':"к", '+84':"е", '+89':"н", '+85':"г", '+73':"ш", '+79':"щ", '+80':"з", '+219':"[", '+221':"]", '+220':"Backslash", '+46':"Удалить"};
@@ -116,7 +116,7 @@ function initKeyLineOne(keyLine) {
     }else if(keyLine[key] === "Delete") {
       out +='<div class="keyLine" data="'+"Delete"+'"'+'>'+'Del'+'</div>';
     }else if(keyLine[key] === "Space") {
-      out +='<div class="keyLine" data="'+"Space"+'"'+'>'+''+'</div>';
+      out +='<div class="keyLineSpace" data="'+"Space"+'"'+'>'+''+'</div>';
     }else {out +='<div class="keyLine" data="'+keyLine[key]+'"'+'>'+keyLine[key].toString()+'</div>';
   }
   }
